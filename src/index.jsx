25d1d23b97948a4bc875d48ca2917432425e07d0
niltoneapontes <niltoneapontes@gmail.com>
2021-05-12 11:12:10 -0300

    first react usage

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,5 @@
 import React from 'react';
-import { App } from './App'
+import { render } from 'react-dom';
+import { App } from './App';
+
+render(<h1>Teste</h1>, document.getElementById('root'));
