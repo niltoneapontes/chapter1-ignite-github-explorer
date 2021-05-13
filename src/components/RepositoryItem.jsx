553fd3b77f717b45b785dcccf9441851ee403f58
niltoneapontes <niltoneapontes@gmail.com>
2021-05-13 09:30:35 -0300

    finish webpack config and init react project

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>unform</strong>
+      <p>Forms em ReactJS</p>
+      <a href="">
+        Acessar repositório
+      </a>
+    </li>
+  )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // importa o plugin
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  mode: isDevelopment ? 'development' : 'production', // auxilia durante o desenvolvimento
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'), // entrada do webpack
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,12 +15,29 @@ module.exports = {
     // extensões que não precisarão ser configuradas
     extensions: ['.js', '.jsx'], // extensões a serem "resolvidas"
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/, 
         exclude: /node_modules/, //node modules nao deve ser testada
         use: 'babel-loader' //precisa instalar
+      },
+      {
+        test: /\.scss$/, 
+        exclude: /node_modules/, //node modules nao deve ser testada
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ] //precisa instalar
       }
     ]
   }
